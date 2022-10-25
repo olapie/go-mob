fa9b53ccb5ff4d3c84a14ec6834f58379d866e49
yxz6 <yxzbox@gmail.com>
2022-10-24 20:49:04 -0400

    Make directories

--- dir.go
+++ dir.go
@@ -1,9 +1,31 @@
 package mobile
 
+import "code.olapie.com/log"
+
 type DirInfo struct {
 	Document  string
 	Cache     string
 	Temporary string
+}
+
+func (d *DirInfo) MustMakeDirs() {
+	if d.Document != "" {
+		MustMkdir(d.Document)
+	} else {
+		log.S().Warn("Document directory is not specified")
+	}
+
+	if d.Cache != "" {
+		MustMkdir(d.Cache)
+	} else {
+		log.S().Warn("Cache directory is not specified")
+	}
+
+	if d.Temporary != "" {
+		MustMkdir(d.Temporary)
+	} else {
+		log.S().Warn("Temporary directory is not specified")
+	}
 }
 
 func NewDirInfo() *DirInfo {
