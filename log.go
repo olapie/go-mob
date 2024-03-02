73f678c2b3e7475f8a7c591e9b7bbbf4fc63a44a
yxz <yxzbox@gmail.com>
2024-03-02 00:05:41 -0500

    fast commit

---
+++ log.go
@@ -0,0 +1,25 @@
+package mob
+
+import "log/slog"
+
+var logger = slog.Default()
+
+func SetLogger(l *slog.Logger) {
+	logger = l
+}
+
+func LogDebug(s string) {
+	logger.Debug(s)
+}
+
+func LogInfo(s string) {
+	logger.Info(s)
+}
+
+func LogWarn(s string) {
+	logger.Warn(s)
+}
+
+func LogError(s string) {
+	logger.Error(s)
+}
